6b9c9d89a9290f397066326df14735ccb7f71f82
east <east0616@163.com>
2023-03-04 14:16:16 +0800

    1

--- server.mjs
+++ server.mjs
@@ -1,6 +1,22 @@
 import { createServer } from 'http';
-
-createServer((req, res) => {
-  res.write('Hello World!');
+import { ChatGPTAPI } from "chatgpt"
+const api = new ChatGPTAPI({
+  apiKey: "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",
+  userLabel: " ",
+  completionParams: {
+    temperature: 0.7,
+    max_tokens: 512,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+  },
+})
+createServer(async (req, res) => {
+  const r = await api.sendMessage(res.text, {
+    promptPrefix: " ",
+    promptSuffix: ".",
+    timeoutMs: 10 * 60 * 1000,
+  })
+  res.json(r)
   res.end();
 }).listen(process.env.PORT);
