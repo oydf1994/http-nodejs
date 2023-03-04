caeebbdd8a99a6a14aba9ecad62b8cfd4c54c034
east <east0616@163.com>
2023-03-04 17:29:50 +0800

    koa

--- app.js
+++ app.js
@@ -1,15 +1,9 @@
 const Koa = require('koa')
 const app = new Koa()
-const views = require('koa-views')
 const json = require('koa-json')
 const onerror = require('koa-onerror')
 const bodyparser = require('koa-bodyparser')
-const logger = require('koa-logger')
-const compress = require('koa-compress');
-const sse = require('koa-sse-stream');
 const index = require('./routes/index')
-const users = require('./routes/users')
-app.use(compress())
 // error handler
 onerror(app)
 
@@ -17,29 +11,10 @@ onerror(app)
 app.use(bodyparser({
   enableTypes: ['json', 'form', 'text']
 }))
-app.use(sse({
-  maxClients: 5000,
-  pingInterval: 30000
-}));
 app.use(json())
-app.use(logger())
-app.use(require('koa-static')(__dirname + '/public'))
-
-app.use(views(__dirname + '/views', {
-  extension: 'pug'
-}))
-
-// logger
-app.use(async (ctx, next) => {
-  const start = new Date()
-  await next()
-  const ms = new Date() - start
-  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
-})
 
 // routes
 app.use(index.routes(), index.allowedMethods())
-app.use(users.routes(), users.allowedMethods())
 
 // error-handling
 app.on('error', (err, ctx) => {
